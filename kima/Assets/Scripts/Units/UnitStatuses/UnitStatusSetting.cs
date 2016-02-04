0a8a2af4201536b5534006f551a1497df6a4f395
一言一行 <everywordandact@gmail.com>
2016-02-04 23:28:27 +0900

    create UnitStatusSetting

---
+++ kima/Assets/Scripts/Units/UnitStatuses/UnitStatusSetting.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace Kima.Units.Statuses
+{
+    [CreateAssetMenu(fileName = "new Unit Setting", menuName = "ScriptableObjests/UnitStatusSetting")]
+    public class UnitStatusSetting
+    {
+        [SerializeField]
+        private int maxHp;
+
+        [SerializeField]
+        private int maxMp;
+
+        public int MaxHp { get { return maxHp; } }
+        public int MaxMp { get { return maxMp; } }
+    }
+}
