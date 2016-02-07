f2b5259af3b39cf9369e635b9061c01295da11da
一言一行 <everywordandact@gmail.com>
2016-02-08 00:22:24 +0900

    ユニットステータスのインスペクタ修正

---
+++ kima/Assets/Editor/Units/Statuses/UnitStatusHolderInspector.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEditor;
+using Kima.Units.Statuses;
+
+namespace Kima.Editor.Units.Statuses
+{
+    [CustomEditor(typeof(UnitStatusHolder))]
+    public class UnitStatusHolderInspector : UnityEditor.Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+
+            var unitStatusHolder = target as UnitStatusHolder;
+
+            EditorGUILayout.LabelField(
+                "HP",
+                unitStatusHolder.Hp.Current.ToString()
+                + " / "
+                + unitStatusHolder.Hp.Max.ToString());
+
+            EditorGUILayout.LabelField(
+                "MP",
+                unitStatusHolder.Mp.Current.ToString()
+                + " / "
+                + unitStatusHolder.Mp.Max.ToString());
+        }
+    }
+}
